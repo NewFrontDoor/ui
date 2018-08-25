a0864a0bcef5c767064f3b91dfcc38aa678a6792
Cameron <cc44@utas.edu.au>
2018-08-25 21:15:54 +1000

    series cards

---
+++ packages/sermon/current-series.fixture.js
@@ -0,0 +1,11 @@
+import CurrentSeries from './current-series';
+
+export default {
+  component: CurrentSeries,
+  props: {
+    header: 'Current Series',
+    seriesTitle: 'Series Title',
+    seriesUrl: 'http://localhost/series_url',
+    artUrl: 'https://vignette.wikia.nocookie.net/gumby/images/c/c7/Gumby_at_his_Desk.jpg'
+  }
+};
---
+++ packages/sermon/featured-series.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card, { CardActions, CardBlock, CardDivider, CardFooter, CardImage, CardTitle } from 'mineral-ui/Card';
+
+class FeaturedSeries extends React.PureComponent {
+  render() {
+  const {header, seriesTitle, seriesUrl, artUrl, ...props} = this.props;
+
+
+    return (
+        <Card>
+            <CardTitle>Featured Series</CardTitle>
+            <CardImage src={this.props.artUrl} alt="Sermon Art" />
+            <CardBlock>{this.props.seriesTitle}</CardBlock>
+        </Card>
+    );
+  }
+}
+
+export default FeaturedSeries;
