ce1c58b6ed68626cbd6f98231b6557e1d381ea84
Alan Reader <areader0@gmail.com>
2019-05-30 16:13:53 +1000

    Adding drupal wrapper

--- packages/calendar/drupal-wrapper.js
+++ packages/calendar/drupal-wrapper.js
@@ -1,3 +1,22 @@
-export function drupalEvents() {
+import React from 'react';
+import PropTypes from 'prop-types';
+import CalendarParent from './calendar-parent';
+import {useFetch} from './utilities/hooks.js';
 
+export function DrupalEvents({url, initialView}) {
+  const [data, loading] = useFetch(url);
+  return (
+    <>
+      {loading ? (
+        'Loading...'
+      ) : (
+        <CalendarParent events={data} initialView={initialView} />
+      )}
+    </>
+  );
 }
+
+DrupalEvents.propTypes = {
+  url: PropTypes.string.isRequired,
+  initialView: PropTypes.oneOf(['day', 'week', 'month']).isRequired
+};
