3e1fbc02a14f9783b00aa40dcd6591e6263a97a5
Alan Reader <areader0@gmail.com>
2019-07-12 15:03:35 +1000

    Adding drupal wrapper

--- packages/calendar/index.js
+++ packages/calendar/index.js
@@ -1,5 +1,7 @@
 import CalendarWrapper from './calendar-wrapper';
 import elvantoEvents from './elvanto-wrapper';
+import drupalEvents from './drupal-wrapper';
 
 export default CalendarWrapper;
 export elvantoEvents;
+export drupalEvents;
