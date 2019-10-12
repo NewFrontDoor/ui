ee88c3e0a8991c075dde73c61e512a03dedf95b3
Jonathan Haines <jonno.haines@gmail.com>
2019-10-12 20:20:05 +1100

    more sermon series

--- packages/sermon/__fixtures__/latest-sermon-container-drupal.fixture.js
+++ packages/sermon/__fixtures__/latest-sermon-container-drupal.fixture.js
@@ -1,10 +1,26 @@
 import config from 'react-global-configuration';
-import {LatestSermonContainerDrupal} from '../src';
+import {
+  LatestSermonContainerDrupal,
+  CurrentSeriesContainerDrupal,
+  RecentSeriesContainerDrupal,
+  SeriesSermonListContainerDrupal
+} from '../src';
 
 config.set({
   DRUPAL_BASE_API_URL: 'https://cornerstoneapi.newfrontdoor.org/api/views/'
 });
 
-export default {
-  component: LatestSermonContainerDrupal
-};
+export default [
+  {
+    component: LatestSermonContainerDrupal
+  },
+  {
+    component: CurrentSeriesContainerDrupal
+  },
+  {
+    component: RecentSeriesContainerDrupal
+  },
+  {
+    component: SeriesSermonListContainerDrupal
+  }
+];
