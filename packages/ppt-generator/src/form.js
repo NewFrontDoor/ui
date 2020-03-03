ec6db0b9e34f26f53fce7844d8681efe948c29ef
readeral <alan@readeral.com>
2020-03-03 12:38:31 +1100

    removing mineral ui from sermons

---
+++ packages/ppt-generator/src/form.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Styled} from 'theme-ui';
+import {Button} from '@theme-ui/components';
+import pptxgen from 'pptxgenjs';
+
+export default function Form({title, description, bgimg, mainbg, contents}) {
+  const pptx = new pptxgen();
+  pptx.title = 'PptxGenJS Sample Presentation';
+  pptx.layout = 'LAYOUT_16x10';
+
+  pptx.defineSlideMaster({
+    title: 'TITLE_SLIDE',
+    objects: [
+      {image: {x: 0, y: 0, w: '100%', h: '100%', path: bgimg}},
+      {
+        placeholder: {
+          options: {
+            name: 'title',
+            type: 'title',
+            x: 1.22,
+            y: 0.76,
+            w: 8.46,
+            h: 3.07,
+            align: 'left',
+            bold: true,
+            fontSize: 44,
+            fontFace: 'Arial',
+            color: 'D6FEFF',
+            shadow: {
+              angle: 45,
+              blur: 3,
+              offset: 3
+            }
+          },
+          text: 'title here'
+        }
+      },
+      {
+        placeholder: {
+          options: {
+            name: 'hymnNumber',
+            type: 'body',
+            x: 1.28,
+            y: 2.85,
+            w: 7,
+            h: 1.6,
+            align: 'left',
+            bold: true,
+            fontSize: 21,
+            fontFace: 'Arial',
+            color: 'FFFFFF',
+            shadow: {
+              angle: 45,
+              blur: 3,
+              offset: 3
+            }
+          },
+          text: 'hymn number here'
+        }
+      }
+    ]
+  });
+
+  pptx.defineSlideMaster({
+    title: 'LYRIC_SLIDE',
+    bkgd: {path: mainbg},
+    objects: [
+      {
+        placeholder: {
+          options: {
+            name: 'body',
+            type: 'body',
+            x: 0.17,
+            y: 0.21,
+            w: 9.58,
+            h: 5.42,
+            align: 'left',
+            fontSize: 37,
+            bold: true,
+            fontFace: 'Arial',
+            color: 'FFFFFF',
+            shadow: {
+              angle: 45,
+              blur: 3,
+              offset: 3
+            }
+          },
+          text: 'Lyrics here'
+        }
+      }
+    ]
+  });
+
+  function produceSlide() {
+    pptx
+      .addSlide('TITLE_SLIDE')
+      .addText('A Charge To Keep I Have', {placeholder: 'title'})
+      .addText('Rejoice 464', {placeholder: 'hymnNumber'});
+    contents.map(para =>
+      pptx.addSlide('LYRIC_SLIDE').addText(para.body, {placeholder: 'body'})
+    );
+    pptx.writeFile('react-demo.pptx');
+  }
+
+  return (
+    <div>
+      <Styled.h2>{title}</Styled.h2>
+      <Styled.p>{description}</Styled.p>
+      <Button onClick={produceSlide}>Click Me</Button>
+    </div>
+  );
+}
+
+Form.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+};
--- packages/sermon/src/series-sermon-list.js
+++ packages/sermon/src/series-sermon-list.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Table} from 'mineral-ui';
 import styled from '@emotion/styled';
 import {PulseLoader} from 'react-spinners';
 import {FaDownload} from 'react-icons/fa';
+import {useTable} from 'react-table';
 
 const Root = styled('td')(({theme}) => ({
   padding: theme.space_stack_sm + ' ' + theme.space_inline_md
@@ -20,28 +20,59 @@ const CustomCell = React.memo(props => {
 
 const cell = ({props}) => <CustomCell {...props} />;
 
-const celt = ({props}) => <CustomCell {...props} />;
+const Table = ({data}) => {
+  const columns = [
+    {Header: 'Title', accessor: 'node_title'},
+    {Header: 'Preacher', accessor: 'preacher'},
+    {Header: 'Date Preached', accessor: 'datepreached'},
+    {Header: 'Sermon', accessor: 'url', Cell: cell}
+  ];
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({
+    data,
+    columns
+  });
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 export default function SermonSeriesList({loading, series, seriesSermonList}) {
-  console.log(seriesSermonList);
-  const columns = [
-    {content: 'Title', key: 'title', cell: {celt}},
-    {content: 'Preacher', key: 'preacher'},
-    {content: 'Date Preached', key: 'datePreached'},
-    {content: 'Sermon', key: 'sermonUrl', cell}
-  ];
   return (
     <section>
       <PulseLoader loading={loading} size={10} />
       {loading || (
         <section>
-          <Table
-            striped
-            columns={columns}
-            data={seriesSermonList}
-            rowKey="title"
-            title={series ? series.title : 'All Sermons'}
-          />
+          <Table data={seriesSermonList} />
         </section>
       )}
     </section>
