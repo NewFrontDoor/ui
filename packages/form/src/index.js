1a60356f958dad890b1537d4b6992699dedcf2d9
readeral <alan@readeral.com>
2020-07-20 21:22:47 +1000

    adjusting package to include sanity required files

--- packages/form/src/index.js
+++ packages/form/src/index.js
@@ -2,7 +2,3 @@ export {default as Form} from './form';
 export {getFormField} from './form';
 export {default as RegexLookup} from './regex-lookup';
 export {default as validation} from './validation';
-
-export {default as formObject} from './schemas/form';
-export {default as formField} from './schemas/formfield';
-export {default as formArrayField} from './schemas/formarrayfield';
