14b2b84bde8398b1abae472d94a94aedfa49eb3e
Andrew Gibb <andrew.gibb@geometryit.com>
2020-07-27 20:17:34 +1000

    Basic layout with links and icons

--- packages/footer/src/footer.tsx
+++ packages/footer/src/footer.tsx
@@ -1,20 +1,80 @@
 /** @jsx jsx */
-import {jsx, Flex, Box, Heading} from 'theme-ui';
+import {jsx, Flex, Box, Image, Link} from 'theme-ui';
 import {FC} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEnvelope, faPodcast} from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebook,
+  faVimeo,
+  faTwitter,
+  faGithub
+} from '@fortawesome/free-brands-svg-icons';
 
-export type FooterProps = {
-  width?: string;
-};
-
-const Footer: FC<FooterProps> = () => {
+const Footer: FC = () => {
   return (
-    <Box p={4} bg="highlight">
+    <Box
+      p={2}
+      sx={{
+        color: 'white',
+        bg: 'black'
+      }}
+    >
       <Flex
         sx={{
-          alignItems: 'center'
+          justifyContent: 'center'
         }}
       >
-        <Heading>Hello world</Heading>
+        <Link
+          p={2}
+          color="white"
+          title="Facebook"
+          href="https://www.facebook.com/NewFrontDoorIT/"
+        >
+          <FontAwesomeIcon icon={faFacebook} size="lg" />
+        </Link>
+        <Link
+          p={2}
+          color="white"
+          title="Vimeo"
+          href="https://vimeo.com/user114967320"
+        >
+          <FontAwesomeIcon icon={faVimeo} size="lg" />
+        </Link>
+        <Link
+          p={2}
+          color="white"
+          title="Contact us"
+          href="contactus@newfrontdoor.org"
+        >
+          <FontAwesomeIcon icon={faEnvelope} size="lg" />
+        </Link>
+        <Link
+          p={2}
+          color="white"
+          title="Twitter"
+          href="https://twitter.com/NewFrontDoorIT"
+        >
+          <FontAwesomeIcon icon={faTwitter} size="lg" />
+        </Link>
+        <Link
+          p={2}
+          color="white"
+          title="Github"
+          href="https://github.com/NewFrontDoor"
+        >
+          <FontAwesomeIcon icon={faGithub} size="lg" />
+        </Link>
+        <Link
+          p={2}
+          color="white"
+          title="Podcast"
+          href="https://podcasts.apple.com/au/podcast/new-front-door-the-church-it-guild/id1477414931"
+        >
+          <FontAwesomeIcon icon={faPodcast} size="lg" />
+        </Link>
+      </Flex>
+      <Flex sx={{justifyContent: 'center'}}>
+        <Box>© New Front Door {new Date().getFullYear()}</Box>
       </Flex>
     </Box>
   );
