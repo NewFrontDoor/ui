b175565bcd2281b7cca2a55b1b2064d5e0db1715
Jonathan Haines <jonno.haines@gmail.com>
2020-12-27 09:50:05 +1100

    Fix tests

--- packages/carousel/__fixtures__/autoplay-on.tsx
+++ packages/carousel/__fixtures__/autoplay-on.tsx
@@ -2,25 +2,25 @@
 import {jsx, css} from 'theme-ui';
 import Carousel from '../src';
 
-const slide = css`
-  flex: 0 0 auto;
-  width: 80%;
-  position: relative;
-  padding-left: 10%;
-  counter-increment: my-counter;
-  div {
-    background-color: rgb(40, 44, 52);
-    position: relative;
-    border-radius: 0.5rem;
-    min-height: 200px;
-    padding-bottom: 46%;
-    font-size: 5rem;
-    color: white;
-    font-weight: 300;
-    line-height: 1;
-    text-align: center;
+const slide = css({
+  flex: '0 0 auto',
+  width: '80%',
+  position: 'relative',
+  paddingLeft: '10%',
+  counterIncrement: 'my-counter',
+  div: {
+    backgroundColor: 'rgb(40, 44, 52)',
+    position: 'relative',
+    borderRadius: '0.5rem',
+    minHeight: '200px',
+    paddingBottom: '46%',
+    fontSize: '5rem',
+    color: 'white',
+    fontWeight: 300,
+    lineHeight: 1,
+    textAlign: 'center'
   }
-`;
+});
 
 export default (
   <Carousel autoplay delayLength={2000}>
